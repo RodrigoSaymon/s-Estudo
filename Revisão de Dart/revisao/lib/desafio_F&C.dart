c59d442d2237a42e26cd9e3fafa3bd388ac71916
Rodrigo Saymon Fragoso Borges <rodrigotbass@gmail.com>
2022-07-11 11:45:51 -0300

    atualizar

--- Revisão de Dart/revisao/lib/desafio_F&C.dart
+++ Revisão de Dart/revisao/lib/desafio_F&C.dart
@@ -1,5 +1,4 @@
 void main() {
-  
   //! Detalhe sobre a String
   //! A String é composta por 3 campos (Nome|Idade|Sexo)
   final pessoas = [
@@ -19,10 +18,67 @@ void main() {
     'Helena|24|Feminino',
     'Laura|29|Feminino',
   ];
+  print('Remova os pacientes duplicados e apresente a nova lista');
+  var pessoasNova = pessoas.toSet();
+  pessoasNova.forEach(print);
+  print('');
+  print(
+      'Me mostre a quantidade de pessoas por sexo (Masculino e Feminino) e depois me apresente o nome delas');
+  var Masculino = [];
+  var Feminino = [];
 
-  //! Baseado na lista acima.
-  //! 1 - Remova os pacientes duplicados e apresente a nova lista
-  //! 2 - Me mostre a quantidade de pessoas por sexo (Masculino e Feminino) e depois me apresente o nome delas
-  //! 3 - Filtrar e deixar a lista somente com pessoas maiores de 18 anos e apresente essas pessoas pelo nome 
-  //! 4 - Encontre a pessoa mais velha e apresente o nome dela.
+  for (var pessoa in pessoasNova) {
+    var dados = pessoa.split('|');
+    var nome = dados[0].toLowerCase();
+    var sexo = dados[2].toLowerCase();
+
+    if (sexo == 'masculino') {
+      Masculino.add(nome);
+    } else {
+      Feminino.add(nome);
+    }
+  }
+  print('${Masculino.length} pessoas do sexo Masculino');
+  Masculino.forEach(print);
+  print('');
+  print('${Feminino.length} pessoas do sexo Feminino');
+  Feminino.forEach(print);
+  print('');
+  print(
+      'Filtrar e deixar a lista somente com pessoas maiores de 18 anos e apresente essas pessoas pelo nome');
+  var maiores = [];
+
+  for (var pessoa in pessoasNova) {
+    var dados = pessoa.split('|');
+    var nome = dados[0].toLowerCase();
+    int idade = int.parse(dados[1]);
+    var dados2 = nome + ', ' + idade.toString();
+
+    if (idade >= 18) {
+      maiores.add(dados2);
+    }
+  }
+  print('${maiores.length} pessoas maiores de 18 anos');
+  maiores.forEach(print);
+
+  print('');
+  print('Encontre a pessoa mais velha e apresente o nome dela');
+
+  var maisVelha = [];
+
+  for (var pessoa in pessoasNova) {
+    var dados = pessoa.split('|');
+    var nome = dados[0].toLowerCase();
+    int idade = int.parse(dados[1]);
+    String veia = idade.toString() + ', ' + nome;
+
+    if (idade >= 0) {
+      maisVelha.add(veia);
+    }
+  }
+  maisVelha.sort();
+
+  var campeao = maisVelha.toList().last;
+
+  print(campeao);
 }
