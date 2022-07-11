e82091ae00997327269482ecbc8f0670d357eb26
Rodrigo Saymon Fragoso Borges <rodrigotbass@gmail.com>
2022-07-11 15:39:00 -0300

    atualizar

--- testando/lib/main.dart
+++ testando/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +7,7 @@ void main() {
 
 class Rodrigo extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(Object context) {
     return MaterialApp(
       home: Saymon(),
     );
@@ -19,9 +20,9 @@ class Saymon extends StatefulWidget {
 }
 
 class SaymonPage extends State<Saymon> {
+  int contar = 0;
   Widget item = Container();
-  var itens = <Widget>[];
-  
+  List itens = <Widget>[];
 
   void add() {
     setState(() {
@@ -35,7 +36,6 @@ class SaymonPage extends State<Saymon> {
     });
   }
 
-  int contar = 0;
   void contando() {
     setState(() {
       contar++;
@@ -52,71 +52,67 @@ class SaymonPage extends State<Saymon> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text('Contando'),
+        backgroundColor: Colors.teal,
+        title: Text(
+          'Contando e Descontando',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
         actions: [
           IconButton(
-            onPressed: () {
-              add();
-            },
-            icon: Icon(Icons.add),
-          ),
+              onPressed: () {
+                add();
+              },
+              icon: Icon(Icons.add)),
           IconButton(
-            onPressed: () {
-              remover();
-            },
-            icon: Icon(Icons.remove),
-          ),
-          SizedBox(
-            width: 10,
-          ),
+              onPressed: () {
+                remover();
+              },
+              icon: Icon(Icons.remove)),
+          SizedBox(width: 10),
         ],
       ),
       body: ListView.builder(
-        itemCount: itens.length,
-        itemBuilder: ((context, index) => ListTile(
-              leading: CircleAvatar(
-                child: Text('$index'),
-              ),
-              title: Text('$contar'),
-              subtitle: Text('previsto ${index * 100}'),
-              trailing: Container(
-                width: 100,
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          contando();
-                        },
-                        icon: Icon(
-                          Icons.edit,
-                          color: Colors.orange,
-                        )),
-                    IconButton(
-                        onPressed: () {
-                          print(itens);
-                          descontando();
-                        },
-                        icon: Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ))
-                  ],
+          itemCount: itens.length,
+          itemBuilder: ((context, index) => Container(
+            color: Colors.teal,
+         
+            
+            
+            child: ListTile(
+                leading: CircleAvatar(
+                  child: Text('$index'),
                 ),
-              ),
-            )),
-      ),
+                title: Text('Despesa $contar'),
+                subtitle: Text('previsto ${index * 100}'),
+                trailing: Container(
+                  width: 100,
+                  child: Row(
+                    children: [
+                      
+                      IconButton(
+                          onPressed: () {
+                            contando();
+                          },
+                          icon: Icon(
+                            Icons.edit,
+                            color: Colors.orange,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            descontando();
+                          },
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          )),
+                    ],
+                  ),
+                )),
+          ))),
     );
   }
 }
 
-
-
-
-
-
-// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
 
 // void main() {
@@ -139,12 +135,12 @@ class SaymonPage extends State<Saymon> {
 
 // class SaymonPage extends State<Saymon> {
 //   Widget item = Container();
+//   var itens = <Widget>[];
+  
 
-//   var itens = <Widget>[Container()];
 //   void add() {
 //     setState(() {
 //       itens.add(item);
-//       print(itens);
 //     });
 //   }
 
@@ -155,7 +151,6 @@ class SaymonPage extends State<Saymon> {
 //   }
 
 //   int contar = 0;
-
 //   void contando() {
 //     setState(() {
 //       contar++;
@@ -170,60 +165,180 @@ class SaymonPage extends State<Saymon> {
 
 //   @override
 //   Widget build(BuildContext context) {
-//     final mediaQuery = MediaQuery.of(context).size;
-
 //     return Scaffold(
-//         appBar: AppBar(
-//           title: Center(
-//             child: Text('contar'),
-//           ),
-//           actions: [
-//             IconButton(
-//                 onPressed: () {
-//                   add();
-//                 },
-//                 icon: Icon(Icons.add)),
-//             IconButton(
-//                 onPressed: () {
-//                   remover();
-//                 },
-//                 icon: Icon(Icons.remove)),
-//             SizedBox(
-//               width: 10,
-//             ),
-//           ],
+//       appBar: AppBar(
+//         title: Center(
+//           child: Text('Contando'),
 //         ),
-//         body: ListView.builder(
-//             itemCount: itens.length,
-//             itemBuilder: ((context, index) => ListTile(
-//                   leading: CircleAvatar(
-//                     child: Text('$index'),
-//                   ),
-//                   title: Text('$contar'),
-//                   subtitle: Text('previsto ${index * 100}'),
-//                   trailing: Container(
-//                     width: 100,
-//                     child: Row(
-//                       children: [
-//                         IconButton(
-//                           onPressed: () {
-//                             descontando();
-//                           },
-//                           icon: Icon(
-//                             Icons.edit,
-//                           ),
-//                         ),
-//                         IconButton(
-//                           onPressed: () {
-//                             contando();
-//                           },
-//                           icon: Icon(
-//                             Icons.delete,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ))));
+//         actions: [
+//           IconButton(
+//             onPressed: () {
+//               add();
+//             },
+//             icon: Icon(Icons.add),
+//           ),
+//           IconButton(
+//             onPressed: () {
+//               remover();
+//             },
+//             icon: Icon(Icons.remove),
+//           ),
+//           SizedBox(
+//             width: 10,
+//           ),
+//         ],
+//       ),
+//       body: ListView.builder(
+//         itemCount: itens.length,
+//         itemBuilder: ((context, index) => ListTile(
+//               leading: CircleAvatar(
+//                 child: Text('$index'),
+//               ),
+//               title: Text('$contar'),
+//               subtitle: Text('previsto ${index * 100}'),
+//               trailing: Container(
+//                 width: 100,
+//                 child: Row(
+//                   children: [
+//                     IconButton(
+//                         onPressed: () {
+//                           contando();
+//                         },
+//                         icon: Icon(
+//                           Icons.edit,
+//                           color: Colors.orange,
+//                         )),
+//                     IconButton(
+//                         onPressed: () {
+//                           print(itens);
+//                           descontando();
+//                         },
+//                         icon: Icon(
+//                           Icons.delete,
+//                           color: Colors.red,
+//                         ))
+//                   ],
+//                 ),
+//               ),
+//             )),
+//       ),
+//     );
 //   }
 // }
+
+
+
+
+
+
+// // import 'package:flutter/cupertino.dart';
+// // import 'package:flutter/material.dart';
+
+// // void main() {
+// //   runApp(Rodrigo());
+// // }
+
+// // class Rodrigo extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       home: Saymon(),
+// //     );
+// //   }
+// // }
+
+// // class Saymon extends StatefulWidget {
+// //   @override
+// //   State<StatefulWidget> createState() => SaymonPage();
+// // }
+
+// // class SaymonPage extends State<Saymon> {
+// //   Widget item = Container();
+
+// //   var itens = <Widget>[Container()];
+// //   void add() {
+// //     setState(() {
+// //       itens.add(item);
+// //       print(itens);
+// //     });
+// //   }
+
+// //   void remover() {
+// //     setState(() {
+// //       itens.remove(item);
+// //     });
+// //   }
+
+// //   int contar = 0;
+
+// //   void contando() {
+// //     setState(() {
+// //       contar++;
+// //     });
+// //   }
+
+// //   void descontando() {
+// //     setState(() {
+// //       contar--;
+// //     });
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     final mediaQuery = MediaQuery.of(context).size;
+
+// //     return Scaffold(
+// //         appBar: AppBar(
+// //           title: Center(
+// //             child: Text('contar'),
+// //           ),
+// //           actions: [
+// //             IconButton(
+// //                 onPressed: () {
+// //                   add();
+// //                 },
+// //                 icon: Icon(Icons.add)),
+// //             IconButton(
+// //                 onPressed: () {
+// //                   remover();
+// //                 },
+// //                 icon: Icon(Icons.remove)),
+// //             SizedBox(
+// //               width: 10,
+// //             ),
+// //           ],
+// //         ),
+// //         body: ListView.builder(
+// //             itemCount: itens.length,
+// //             itemBuilder: ((context, index) => ListTile(
+// //                   leading: CircleAvatar(
+// //                     child: Text('$index'),
+// //                   ),
+// //                   title: Text('$contar'),
+// //                   subtitle: Text('previsto ${index * 100}'),
+// //                   trailing: Container(
+// //                     width: 100,
+// //                     child: Row(
+// //                       children: [
+// //                         IconButton(
+// //                           onPressed: () {
+// //                             descontando();
+// //                           },
+// //                           icon: Icon(
+// //                             Icons.edit,
+// //                           ),
+// //                         ),
+// //                         IconButton(
+// //                           onPressed: () {
+// //                             contando();
+// //                           },
+// //                           icon: Icon(
+// //                             Icons.delete,
+// //                           ),
+// //                         ),
+// //                       ],
+// //                     ),
+// //                   ),
+// //                 ))));
+// //   }
+// // }
